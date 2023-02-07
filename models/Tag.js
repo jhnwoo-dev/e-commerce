b7d1f9bf7b929e60d564922f3b4384ca3ebe2544
Jason Nguyen <jhnwoo.dev@gmail.com>
2023-02-07 00:36:35 -0800

    built models

--- models/Tag.js
+++ models/Tag.js
@@ -7,6 +7,18 @@ class Tag extends Model {}
 Tag.init(
     {
         // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        // product_id: {
+        //     type: DataTypes.INTEGER,
+        // },
+        // tag_id: {
+        //     type: DataTypes.INTEGER,
+        // },
     },
     {
         sequelize,
